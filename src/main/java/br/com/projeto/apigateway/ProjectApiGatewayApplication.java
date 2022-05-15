4ef37d13f9c0e08e335de30558c193d69306fcda
jhonnyscerni <jhonnyscerni@gmail.com>
2022-05-15 17:37:48 -0300

    # Aoi Gateway

---
+++ src/main/java/br/com/projeto/apigateway/ProjectApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package br.com.projeto.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProjectApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/projeto/apigateway/ProjectApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
