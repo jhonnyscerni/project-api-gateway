e9888e5d9ff9ce8e83b883c758f81f80093a56af
jhonnyscerni <jhonnyscerni@gmail.com>
2022-09-28 01:06:33 -0300

    ajustes e config para ambiente prod

--- src/main/java/br/com/projeto/apigateway/ApiGatewayApplication.java
+++ src/main/java/br/com/projeto/apigateway/ApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ProjectApiGatewayApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectApiGatewayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
